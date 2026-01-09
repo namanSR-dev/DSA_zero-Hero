edb0bde3cbb60b2b305c83a0aad6b45419209ecf
Naman Singh Rathaur <165822591+namanSR-dev@users.noreply.github.com>
2026-01-09 05:56:18 +0000

    pattern 1.1: linear-traversal + conditional-check

--- foundation/linear-traversal-And-operations.py
+++ foundation/linear-traversal-And-operations.py
@@ -15,8 +15,32 @@ def frequency_count (seq:list[int]) -> None :
 
     for x in seq:
        if x in dict:
-        dict[x] = dict[x] + 1
-    else:
-        dict[x] = 1
+          dict[x] = dict[x] + 1
+       else:
+          dict[x] = 1
 
-    print(dict.items())
+    print(dict)
+
+# frequency_count( [4,6,2,4,32,4,6] )
+
+
+
+
+# linear-traversal + Conditional-check
+
+def two_sum ( seq: list[int], target:int ) -> set[int] | None:
+    seen = set() # set is used to check member ship, dictionary can also be used both has O(1) lookup but dict. has memory overhead
+                # [note:- {} will create the empty dictionary not set.]
+    for x in seq:
+      needed = target - x
+      if needed in seen:
+         return (needed, x)
+      seen.add(x)
+    return None
+
+# seq = [5, 7, 9, 35, 8, 2, 57, 45]
+# target = 64
+
+# print(two_sum(seq, target))
+      
+     
