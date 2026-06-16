36ff1a971bbc3962438ed142b957708cabdf5e40
Naman Singh Rathaur <namansingh99694@gmail.com>
2026-06-17 00:56:04 +0530

    Tree : Pattern 4 (Multi-State Return)

--- DSA-Non_linear_pattern/trees/Balanced_binary_Tree.py
+++ DSA-Non_linear_pattern/trees/Balanced_binary_Tree.py
@@ -1,5 +1,6 @@
 from tree_implement.tree_base import Tree
 
+#Pattern 4:  Multi-state return
 def check_balance(node):
 
     if not node:
@@ -8,7 +9,7 @@ def check_balance(node):
     left_height, is_left_balanced = check_balance(node.left)
     right_height, is_right_balanced = check_balance(node.right)
 
-    return 1 + max(left_height, right_height), abs(left_height - right_height) <= 1
+    return 1 + max(left_height, right_height),  is_left_balanced and is_right_balanced and abs(left_height - right_height) <= 1
 
 
 
@@ -16,7 +17,7 @@ if __name__ == "__main__":
 
     mytree  = Tree()
 
-    mytree.create_tree([1, 2, None, 4, 5])
+    mytree.create_tree([1, 2, 3, 4, None, None, 5, 6, None, None, 7])
 
     mytree.print_tree()
 
